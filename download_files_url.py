3799d5ea565259fed29525d605092baf44734505
senabayraktaroglu@novit.ai <senabayraktaroglu@novit.ai>
2020-11-11 14:16:31 +0300

    ADD 5. Milestone Read URL's from an excel file using pandas.Print what is being downloaded using another column in pandas.Download those files and check them

--- download_files_url.py
+++ download_files_url.py
@@ -58,7 +58,7 @@ for i in range(0,len(download_links_list)):
 
 """            
 #Step 4
-
+"""
 path_for_excel = "different_datasets.xlsx"
 excel_file_in =  pd.read_excel(path_for_excel)
 print(list(excel_file_in.columns).index('Downloadable Link'))
@@ -86,4 +86,39 @@ for i in range(0,len(download_links_list)):
         except:
             downloaded_without_problem.append("File not exist")
             
+"""
+
+#Step 5
+path_for_excel = "different_datasets.xlsx"
+excel_file_in =  pd.read_excel(path_for_excel)
+print(list(excel_file_in.columns).index('Downloadable Link'))
+download_links_list = excel_file_in['Downloadable Link']
+names_of_datasets = list(excel_file_in['Name'])
+print(download_links_list)
+download_links_list = list(download_links_list)
+print(download_links_list)
+downloaded_without_problem = []
+for i in range(0,len(download_links_list)):
+    temp_url = download_links_list[i]
+    if temp_url.find("kaggle") != -1:
+       print("do kaggle download ", temp_url)
+       downloaded_without_problem.append("do kaggle download ")
+       
+    else:
+        try:
+            temp_data = wget.download(temp_url)
+            print("name_of_file :", temp_data)
+            if Path(temp_data).is_file():
+                print ("File exist")
+                print("Name of data set which is downloaded: ",names_of_datasets[i])
+                downloaded_without_problem.append("File exist")
+            else:
+                print ("File not exist")
+                downloaded_without_problem.append("File not exist")
+        except:
+            downloaded_without_problem.append("File not exist")
             
+#excel_file_out = excel_file_in.assign(download_situation = downloaded_without_problem) 
+#print(excel_file_out.head)
+#excel_file_out.to_excel("output.xlsx")  
+
