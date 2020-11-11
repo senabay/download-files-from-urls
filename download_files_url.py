0fc1b3e15a1e3708abc89a091fdbf0de20e64a4f
senabayraktaroglu@novit.ai <senabayraktaroglu@novit.ai>
2020-11-11 13:54:44 +0300

    ADD 3. Milestone Download multiple files, from a list. Check files are downloaded using pathlib Path

--- download_files_url.py
+++ download_files_url.py
@@ -9,7 +9,7 @@ temp_data = wget.download(temp_url)
 """
 
 #Step 2
-
+"""
 download_links_list= ['https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls','http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv'
                 ,'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL.FE.IN?downloadformat=csv',
                 'http://api.worldbank.org/v2/en/indicator/SP.POP.DPND?downloadformat=csv']
@@ -28,3 +28,26 @@ for i in range(0,len(download_links_list)):
         print("name_of_file :", temp_data)
 
 
+"""
+
+#Step 2
+from pathlib import Path
+download_links_list= ['https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls','http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv'
+                ,'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL.FE.IN?downloadformat=csv',
+                'http://api.worldbank.org/v2/en/indicator/SP.POP.DPND?downloadformat=csv']
+
+
+
+for i in range(0,len(download_links_list)):
+    temp_url = download_links_list[i]
+    if temp_url.find("kaggle") != -1:
+       print("do kaggle download ", temp_url)
+       downloaded_without_problem.append("do kaggle download ")
+
+    else:   
+        temp_data = wget.download(temp_url)
+        print("name_of_file :", temp_data)
+        if Path(temp_data).is_file():
+            print ("File exist")
+        else:
+            print ("File not exist")
